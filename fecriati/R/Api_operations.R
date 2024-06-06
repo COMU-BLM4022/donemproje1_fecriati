39721dc0cda3a455f8a0f4286595e09cbdb48078
Mustafa Kocer <mustafakoceerr@hotmail.com>
2024-06-07 02:37:01 +0300

    Asama2Q4Tamamlandi
    proje bozuk olduğu için datalar değiştirilmeden yeni projeye aktarıldı

---
+++ fecriati/R/Api_operations.R
@@ -0,0 +1,16 @@
+#' Api ile veri yazma islemi
+#' @export
+post_api <- function() {
+  # bu islemler sonraki asamada tanimlanacak
+  # yapildi, düzenlenilmeyi bekliyorlar
+
+}
+
+
+#' Api ile veri cekme islemi
+#' @export
+gets_api <- function() {
+  # bu islemler sonraki asamada tanimlanacak
+  # yapildi, düzenlenilmeyi bekliyorlar
+
+}
